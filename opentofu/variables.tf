2fd5a14314878ccb564c3c0fb1ac2eda3f6a8e70
Forrest Fuqua <804052+cyberbalsa@users.noreply.github.com>
2025-12-31 08:26:46 -0500

    Update image names for Windows and Debian

--- opentofu/variables.tf
+++ opentofu/variables.tf
@@ -2,8 +2,8 @@
 variable "network_name" { default = "cdt-net" }
 variable "subnet_cidr" { default = "10.10.10.0/24" }
 variable "router_name" { default = "cdt-router" }
-variable "windows_image_name" { default = "WinSrv2022-20348-2022" }
-variable "debian_image_name" { default = "debian-trixie-amd64-cloud-20250628-05-24" }
+variable "windows_image_name" { default = "WindowsServer2022" }
+variable "debian_image_name" { default = "Ubuntu2404Desktop" }
 variable "flavor_name" { default = "medium" }
 variable "keypair" { default = "coredesktop" }
 variable "security_group" { default = "main-boxes" }
